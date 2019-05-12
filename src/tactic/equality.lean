411c2bf3c107a8160f71f454129579787a76e0b6
François G. Dorais <fgdorais@gmail.com>
2019-05-12 08:26:08 -0400

    fix: names

--- src/tactic/equality.lean
+++ src/tactic/equality.lean
@@ -3,7 +3,7 @@ Copyright © 2019 François G. Dorais. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 -/
 
-import .groupoids.paths
+import groupoid.paths
 
 namespace tactic
 
